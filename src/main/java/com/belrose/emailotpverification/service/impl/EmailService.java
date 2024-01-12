0b0f16b3db33eec2fe505449a0e9198c53334d8b
Ngnawen Samuel <ngnawens@gmail.com>
2024-01-12 14:05:19 -0500

    config class added and code refactor

--- src/main/java/com/belrose/emailotpverification/service/impl/EmailService.java
+++ src/main/java/com/belrose/emailotpverification/service/impl/EmailService.java
@@ -2,11 +2,13 @@ package com.belrose.emailotpverification.service.impl;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class EmailService {
     private final JavaMailSender javaMailSender;
 
@@ -22,9 +24,9 @@ public class EmailService {
             helper.setSubject(subject);
             helper.setText(body,true);
             javaMailSender.send(message);
+            log.info(String.format("JavaMailSender:sendEmail: %s {} is successfully sent",message));
         } catch (MessagingException e) {
-            e.printStackTrace();
-            e.getMessage();
+            log.error(String.format("JavaMailSender:sendEmail:Error to send OTP to %s. Cause: %s",to,e));
             throw new RuntimeException(e);
         }
     }
